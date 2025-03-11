cb7c39fb45d4106559f2fd4fcf1966d503e604f9
iouvps <94985722+pangsusu@users.noreply.github.com>
2025-03-11 11:26:30 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1892418-a0601bfe8c81add35b5b10f8'
+    'ur1892418-a0601bfe8c81add35b5b10f8',
   ],
 
   // 日志天数
